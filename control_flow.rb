61a267b17d300b51d635e7a3ddd6f8eccb4e7c7f
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-08-12 13:16:52 -0700

    commit tree ed6fcb44519a29062678b33ea1dd56bedb087ce1

--- control_flow.rb
+++ control_flow.rb
@@ -1,54 +1,39 @@
 def admin_login(username, password)
-  # your code here
-  if username == "admin" && password == "12345"
+  if (username == "admin"|| username == "ADMIN") && password == "12345"
     "Access granted"
-  elsif
-    username == "ADMIN" && password =="12345"
-    "Access granted"
-  else
+  else 
     "Access denied"
   end
 end
 
 def hows_the_weather(temperature)
   # your code here
-  
-  
-  if temperature <= 40
+  if temperature < 40
     "It's brisk out there!"
   elsif temperature >= 40 && temperature <= 65
     "It's a little chilly out there!"
-  elsif temperature >= 85
+  elsif temperature > 85
     "It's too dang hot out there!"
   else
     "It's perfect out there!"
   end
-  # case temperature
-  # when < 40
-  #   "It's brisk out there!"
-  # when >= 65
-  #   "It's a little chilly out there!"
-  # when > 85
-  #   "It's too dang hot out there!"
-  # else "It's perfect out there!"
-    
 end
 
 def fizzbuzz(num)
-  # your code here
-  if num % 3 == 0 && num % 5 == 0  
-    "FizzBuzz"
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
   elsif num % 3 == 0
-    "Fizz"
+    return "Fizz"
   elsif num % 5 == 0
-    "Buzz"
+    return "Buzz"
   else
     num
   end
 end
 
-
 def calculator(operation, num1, num2)
+  # operation = "Invalid Operation!"
+
   case operation
   when "+" then num1 + num2
   when "-" then num1 - num2
@@ -58,4 +43,3 @@ def calculator(operation, num1, num2)
     puts "Invalid operation!"
   end
 end
-
